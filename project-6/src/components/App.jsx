d6c601a0a57dd67697144cce497db781f93c3552
Johann Schneider <joschi.dev7@gmail.com>
2026-01-24 21:22:16 +0100

    implemented header and basic status logic

---
+++ project-6/src/components/App.jsx
@@ -0,0 +1,11 @@
+import Header from './Header.jsx'
+import Status from './Status.jsx'
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Status />
+        </div>
+    )
+}
---
+++ project-6/src/components/Status.jsx
@@ -0,0 +1,8 @@
+export default function Status() {
+    return (
+        <div className="status-div">
+            <h2>You Win!</h2>
+            <p>Well done!🎉</p>
+        </div>
+    )
+}
